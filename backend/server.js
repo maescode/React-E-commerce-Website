24867b6c1fc6df22cbf1d4377c64ac7616ac3b09
Olagoke Abel Olayinka <olagokeabel@gmail.com>
2021-11-14 19:28:20 +0100

    add backend and api urls

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const products = require("./data/products");
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
+app.listen(5000, console.log("server running on port 500"));
